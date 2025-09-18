6f85f59731861235c686782b66690d999d6986d7
George Silva <george@agsconsultoria.com.br>
2025-09-18 18:31:01 -0400

    Criação da estrutura de diretorios das views do projeto

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('site.index');
+});
+
+
+Route::get('/painel', function () {
+    return view('painel.index');
+});
+
+
+Route::get('/adm', function () {
+    return view('painel.adm.index');
+});
+
+
+Route::get('/aluno', function () {
+    return view('painel.aluno.index');
+});
+
+
+Route::get('/professor', function () {
+    return view('painel.professor.index');
 });
