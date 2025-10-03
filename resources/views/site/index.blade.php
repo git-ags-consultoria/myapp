34023a09b37975375aa5709337d294ae496c2a31
George Silva <george@agsconsultoria.com.br>
2025-10-03 14:59:41 -0400

    Instalando o Tailwind

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -1,1 +1,13 @@
-Estou no index do site
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-emerald-700 flex justify-center justify-items-center">
+<h1 class="text-3xl text-white font-bold">
+    Estou no Index do Site
+</h1>
+</body>
+</html>
