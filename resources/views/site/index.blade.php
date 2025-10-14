d1c30bd33b1071524fff05ce9eb7bce9fa25235f
George Silva <george@agsconsultoria.com.br>
2025-10-14 08:54:36 -0400

    Teste com a Lizi

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -4,10 +4,11 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     @vite('resources/css/app.css')
+
 </head>
 <body class="bg-emerald-700 flex justify-center justify-items-center">
 <h1 class="text-3xl text-white font-bold">
-    Estou no Index do Site
+    Estou no Index do Site Testando com a Lize
 </h1>
 </body>
 </html>
